1237a69e4c7e524211d8ed9a068f1db19b575e1e
Miriam <rezakmiriam@gmail.com>
2022-03-29 18:48:12 +0200

    [FEATURE] car structure

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,51 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
 
+let auto = {
+  znacka: 'Ford',
+  barva: 'modrá',
+  rokVyroby: 2015,
+  spz: '1B2 3456',
+};
 
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
 
+auto.majitel = {};
+auto.majitel.jmeno = 'Jana';
+auto.majitel.prijmeni = 'Czechita';
 
 // Přepiš značku na Škoda
 
-
+auto['znacka'] = 'Škoda';
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+
+let majitelka = auto.majitel.jmeno + ' ' + auto.majitel.prijmeni;
+let znacka = auto.znacka;
+let spz = auto.spz;
+
+/*
+let poleCisel = [12, 78, 18, 40];
+console.log(poleCisel);
+poleCisel.sort(porovnej);
+console.log(poleCisel);
+
+function porovnej(cis1, cis2) {
+  if (cis1 > cis2) {
+    return 1;
+  } else {
+    return -1;
+  }
+};
+
+
+let poleKamaradu = [
+  { jmeno: 'Vanda', vek: 12},
+  { jmeno: 'Hubert', vek: 78},
+  { jmeno: 'Amalie', vek: 18},
+  { jmeno: 'Olivie', vek: 40},
+];
+
+console.log(poleKamaradu);
+poleKamaradu.sort(porovnejObj);
+console.log(poleKamaradu);
+
+*/
